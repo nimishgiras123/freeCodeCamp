1cfc0bbed7a5330feed1437fb059655cdd3901ed
Nimish Gigras <83623570+nimishgiras123@users.noreply.github.com>
2024-10-21 04:39:54 +0530

    Update learn-alert.tsx
    replaced Spacer Component location : import { Link, Spacer } from '../components/helpers'; and added import { Spacer } from '@freecodecamp/ui';

--- client/src/components/Intro/learn-alert.tsx
+++ client/src/components/Intro/learn-alert.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Alert } from '@freecodecamp/ui';
 import { useFeature } from '@growthbook/growthbook-react';
 import { useTranslation } from 'react-i18next';
-import { Link, Spacer } from '../helpers';
+import { Link } from '../helpers';
+import { Spacer } from '@freecodecamp/ui';
 import { ProgressBar } from '../Progress/progress-bar';
 
 interface LearnAlertProps {
