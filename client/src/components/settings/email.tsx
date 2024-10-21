c173acbf115d989ad9b26a8c01d810c55476dc4a
Nimish Gigras <83623570+nimishgiras123@users.noreply.github.com>
2024-10-21 12:10:16 +0530

    Update email.tsx
    migrated usage of spacer.tsx from @freecodecamp/ui instead of ../helpers/spacer';

--- client/src/components/settings/email.tsx
+++ client/src/components/settings/email.tsx
@@ -5,7 +5,8 @@ import {
   FormGroupProps,
   FormControl,
   ControlLabel,
-  Button
+  Button,
+  Spacer
 } from '@freecodecamp/ui';
 import React, { useState } from 'react';
 import type { TFunction } from 'i18next';
@@ -20,7 +21,6 @@ import { maybeEmailRE } from '../../utils';
 
 import BlockSaveButton from '../helpers/form/block-save-button';
 import FullWidthRow from '../helpers/full-width-row';
-import Spacer from '../helpers/spacer';
 import Link from '../helpers/link';
 import SectionHeader from './section-header';
 import ToggleButtonSetting from './toggle-button-setting';
