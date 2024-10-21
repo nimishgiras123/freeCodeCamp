22bf45ece2ead071df2f2421989188e90b45f659
Nimish Gigras <83623570+nimishgiras123@users.noreply.github.com>
2024-10-21 10:25:22 +0530

    Update index.tsx
    migrated usage of spacer.tsx from @freecodecamp/ui instead of ../helpers/spacer';

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Trans, useTranslation } from 'react-i18next';
-import { Col } from '@freecodecamp/ui';
+import { Col, Spacer } from '@freecodecamp/ui';
 
 import appleStoreBadge from '../../assets/images/footer-ads/apple-store-badge.svg';
 import googlePlayBadge from '../../assets/images/footer-ads/google-play-badge.svg';
-import { Spacer, Link } from '../helpers';
+import { Link } from '../helpers';
 import './footer.css';
 
 function Footer(): JSX.Element {
