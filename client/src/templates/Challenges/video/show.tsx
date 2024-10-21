80de777560adc68fecccf72c1b85adbf2773d585
Nimish Gigras <83623570+nimishgiras123@users.noreply.github.com>
2024-10-21 13:14:44 +0530

    Update show.tsx
    migrated usage of spacer.tsx from @freecodecamp/ui instead of ../helpers/spacer';

--- client/src/templates/Challenges/video/show.tsx
+++ client/src/templates/Challenges/video/show.tsx
@@ -10,10 +10,9 @@ import { bindActionCreators } from 'redux';
 import type { Dispatch } from 'redux';
 import { createSelector } from 'reselect';
 import { isEqual } from 'lodash-es';
-import { Container, Col, Row, Button } from '@freecodecamp/ui';
+import { Container, Col, Row, Button, Spacer } from '@freecodecamp/ui';
 
 // Local Utilities
-import Spacer from '../../../components/helpers/spacer';
 import LearnLayout from '../../../components/layouts/learn';
 import { ChallengeNode, ChallengeMeta, Test } from '../../../redux/prop-types';
 import { challengeTypes } from '../../../../../shared/config/challenge-types';
